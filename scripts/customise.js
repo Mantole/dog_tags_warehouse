4ab2da8dc4ae0cad0ef30f0e48bb7c24ffa5cea4
Mantole <mantole77@gmail.com>
2017-09-24 23:35:27 +0100

    dog_tags_warehouse
    dog_tags_warehouse

---
+++ scripts/customise.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+////
+  // Tag1 1st line
+var $name1_1 = $('#tg1_input1');
+var $value1_1 = $('#tg1_line1');
+
+  $name1_1.on('input', function(event){
+  $value1_1.html($name1_1.val());
+  
+});
+
+// Tag1 2nd line //
+
+  var $name1_2 = $('#tg1_input2');
+  var $value1_2 = $('#tg1_line2');
+
+  $name1_2.on('input', function(event){
+  $value1_2.html($name1_2.val());
+});
+
+// Tag1 3rd line //
+
+  var $name1_3 = $('#tg1_input3');
+  var $value1_3 = $('#tg1_line3');
+
+  $name1_3.on('input', function(event){
+  $value1_3.html($name1_3.val());
+});
+
+
+// Tag1 4th line //
+
+  var $name1_4 = $('#tg1_input4');
+  var $value1_4 = $('#tg1_line4');
+
+  $name1_4.on('input', function(event){
+  $value1_4.html($name1_4.val());
+});
+
+// Tag1 5th line //
+
+  var $name1_5 = $('#tg1_input5');
+  var $value1_5 = $('#tg1_line5');
+
+  $name1_5.on('input', function(event){
+  $value1_5.html($name1_5.val());
+});
+
+//////////////////////////////////////
+
+  // Tag2 1st line
+var $name2_1 = $('#tg2_input1');
+var $value2_1 = $('#tg2_line1');
+
+  $name2_1.on('input', function(event){
+  $value2_1.html($name2_1.val());
+});
+
+// Tag2 2nd line //
+
+  var $name2_2 = $('#tg2_input2');
+  var $value2_2 = $('#tg2_line2');
+
+  $name2_2.on('input', function(event){
+  $value2_2.html($name2_2.val());
+});
+
+// Tag2 3rd line //
+
+  var $name2_3 = $('#tg2_input3');
+  var $value2_3 = $('#tg2_line3');
+
+  $name2_3.on('input', function(event){
+  $value2_3.html($name2_3.val());
+});
+
+
+// Tag2 4th line //
+
+  var $name2_4 = $('#tg2_input4');
+  var $value2_4 = $('#tg2_line4');
+
+  $name2_4.on('input', function(event){
+  $value2_4.html($name2_4.val());
+});
+
+// Tag2 5th line //
+
+  var $name2_5 = $('#tg2_input5');
+  var $value2_5 = $('#tg2_line5');
+
+  $name2_5.on('input', function(event){
+  $value2_5.html($name2_5.val());
+});
+
+});
---
+++ scripts/menu_toggle.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	// toggle navigation on smaller screens //
+  $(".fa-reorder").click(function(){
+	$("#small_screen_navigation").toggle();
+  });
+
+});
